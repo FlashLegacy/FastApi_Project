6880d6d3333d9f1b8928704e64db3f3321e2f90c
Legacy <Legacy.working@yandex.ru>
2023-09-03 02:58:08 +0700

    delete config

--- database/config.py
+++
@@ -1,8 +0,0 @@
-DB_HOST = "localhost" 
-DB_PORT = 5432 
-
-DB_NAME = "postgres"
-DB_USER = "postgres"
-DB_PASS = 11545663
-
-DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
